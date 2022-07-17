9c069c035b239f6bbf20cf5a42729708376ac970
Marlon Sousa <marlon.bsousa@gmail.com>
2022-07-17 18:21:22 +0000

    Initial tests on http requests

--- src/http_request.rs
+++ src/http_request.rs
@@ -1,7 +1,39 @@
+/// represents a http file collection, understandable by RestClient
 pub struct HttpRequests {
+    /// list of requests in collection
     pub requests: Vec<HttpRequest>,
 }
 
+/// represent a http request
 pub struct HttpRequest {
-    name: String,
+    /// request name
+    pub name: String,
+}
+
+impl HttpRequest {
+    /// converts a request to rest client format
+    pub fn to_restclient(&self) -> String {
+        format!("#####\n# @name {}\n", self.name)
+    }
+}
+
+/*
+    see 010
+*/
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use indoc::indoc;
+    #[test]
+    fn http_request_is_generated_in_valid_format() {
+        let result = indoc! {r#"
+            #####
+            # @name testReq
+        "#};
+        let req = HttpRequest {
+            name: "testReq".to_string(),
+        };
+        assert_eq!(req.to_restclient(), result);
+    }
 }
